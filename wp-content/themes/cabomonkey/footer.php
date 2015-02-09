423346a98cdf07e7b05c1ab8b46a4c5fa940d3ef
ericdolphinguy <eric.dolphinguy@gmail.com>
2015-02-09 12:14:53 +0800

    Added footer text and html elements.

--- wp-content/themes/cabomonkey/footer.php
+++ wp-content/themes/cabomonkey/footer.php
@@ -12,12 +12,23 @@
 
 	<footer id="colophon" class="site-footer columns large-12 medium-12" role="contentinfo">
 		<div class="pickup">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/image-5.png" />
+			<div class="image">
+				<img src="<?php /*echo get_stylesheet_directory_uri(); */?>/images/image-10.png" />
+			</div>
+			<div class="text-1">
+				FREE <span>PICKUP & DROP OFF</span>
+			</div>
+			<div class="text-2">
+				Door-to-door shuttle is included in the price of the tour for hotels in Cabo San Lucas, corridor and San Jose Del Cabo as far east as Bla Bla. When you reserve your tour ticket will indicate your pickup
+				location and pickup time. Please read the FAQ about transportation and each tour your plan on booking to get all your questions answered.
+			</div>
 		</div>
 		<div class="site-info">
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet consequatur cupiditate, eos incidunt ipsam
 			nihil perspiciatis reiciendis similique soluta voluptate? Consequatur facere ipsa nobis pariatur possimus
-			praesentium saepe vel vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae deserunt doloremque doloribus, eaque earum necessitatibus tempore tenetur? Asperiores aspernatur cupiditate expedita minima minus nobis nostrum obcaecati qui similique, soluta.
+			praesentium saepe vel vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae
+			deserunt doloremque doloribus, eaque earum necessitatibus tempore tenetur? Asperiores aspernatur cupiditate
+			expedita minima minus nobis nostrum obcaecati qui similique, soluta.
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
