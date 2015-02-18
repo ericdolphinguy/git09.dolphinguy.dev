f5d8c69650a2fe0cc5cca65e2686c981633b1da3
ericdolphinguy <eric.dolphinguy@gmail.com>
2015-02-18 09:27:01 +0800

    Fixed desktop to mobile behaviour response and back of trip advisor widget and small image.

--- wp-content/themes/cabomonkey/js/custom.js
+++ wp-content/themes/cabomonkey/js/custom.js
@@ -7,20 +7,16 @@ jQuery(document).ready(function($){
         $('body,#site-navigation').removeClass('toggled-1');
         $('body,#site-navigation').toggleClass('toggled-2');
     });
+});
 
-    $(window).resize(function(){
-        var width = $(this).width();
+jQuery(window).load(function(){
+    $ = jQuery;
 
-        if ( width <= 1230 ) {
-            $('.offer').each(function(i,e){
-                var coe = $('> .image .boxes .box-2 > div', e).detach();
-                $('.ticket-box .tripadvisor', e).append(coe);
+    $('.offer').each(function(i,e){
+        var coe = $('> .image .boxes .box-2 > div', e).clone( true, true );
+        $('.ticket-box .tripadvisor', e).append(coe);
 
-                var img = $('> .image .boxes .box-1 > img', e).detach();
-                $('.ticket-box .coupon_image', e).append(img);
-            });
-        }
+        var img = $('> .image .boxes .box-1 > img', e).clone( true, true );
+        $('.ticket-box .coupon_image', e).append(img);
     });
-
-    $(window).trigger('resize');
 });
